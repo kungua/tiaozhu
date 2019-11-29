aa68e5e886eddbe99c34ce1b7fe1564b5296aa79
kungua <maguahongkun@outlook.com>
2019-11-29 15:56:55 +0800

    feat: divided webpack.config.js to dev & prod version

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'production',
   entry: {
     tiaozhu: './lib/index.tsx'
   },
@@ -21,25 +19,5 @@ module.exports = {
         loader: 'awesome-typescript-loader'
       }
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "跳珠 React UI Toolkit",
-      template: "index.html"
-    })
-  ],
-  externals: {
-    'react': {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: 'React' // script tag window.React
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-      root: 'ReactDOM'
-    }
   }
 }
